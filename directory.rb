aa4258940b0ca083428863cb9da577528850e578
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-17 06:59:18 +0000

    refactored code

--- directory.rb
+++ directory.rb
@@ -1,40 +1,48 @@
+@students = []
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 beacuse we'llbe adding more items
-    selection = gets.chomp
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
     case selection
       when "1"
         # input the students
         students = input_students
       when "2"
         # show the students
-        print_header
-        print(students)
-        print_footer(students)
+        show_students
       when "9"
         # Exit
         exit
       else
         puts "I don't know what you meant,try again"
     end
-  end
 end
 
+def print_menu
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 beacuse we'llbe adding more items  
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end  
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = [] # created an empty array
   name = gets.chomp.capitalize
   until name.empty? do
-    students.push({name: name, cohort: :november})
-    puts "now we have #{students.count} number of students"
+    @students.push({name: name, cohort: :november})
+    puts "now we have #{@students.count} number of students"
     name = gets.chomp.capitalize
   end
-  students
 end
         
 def print_header
@@ -42,14 +50,14 @@ def print_header
   puts "--------------------------------"
 end
 
-def print(students)
-  students.each do |name|
+def print_students_list
+  @students.each do |name|
   puts "#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students "
+def print_footer
+  puts "Overall, we have #{@students.count} great students "
 end
 
 # nothing happens until we call the methods
